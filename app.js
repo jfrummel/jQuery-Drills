4014bc544d31aeb7496f3626f83a0dceca70823f
jfrummel <42078577+jfrummel@users.noreply.github.com>
2018-09-25 11:25:14 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,4 +1,10 @@
 $('document').ready(function() {
+    $(':input[type="submit"]').prop('disabled', true);
+     $('input[type="text"]').keyup(function() {
+        if($(this).val() != '') {
+           $(':input[type="submit"]').prop('disabled', false);
+        }
+     });
     $('#btnSubmit').click(function() {
         alert('The button has been clicked.');
     });
@@ -7,9 +13,15 @@ $('document').ready(function() {
         event.preventDefault();
         $('<li>' + $('input[type="text"]').val() + '</li>').appendTo('body');
     });
-        
+    $('ul').appendTo('body');
+    
+    let r = Math.floor(Math.random()*255);
+    let b = Math.floor(Math.random()*255);
+    let g = Math.floor(Math.random()*255);
 
-   $('ul').appendTo('body');    
+    $('li').click(function() {
+        $('li').css('background-color', 'rgb(r, b, g)');
+});
 });
 
     
